1723fb0703578fca20046ff7fba3970e4e77a65b
ElianaHarriet <eharriet@fi.uba.ar>
2025-02-01 15:40:22 -0300

    Adding basic files

---
+++ lib/utils.py
@@ -0,0 +1,33 @@
+import datetime
+import os
+import time
+from typing import Optional, Union
+from fastapi import HTTPException
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import logging as logger
+import re
+
+DAY = 24 * 60 * 60
+HOUR = 60 * 60
+MINUTE = 60
+MILLISECOND = 1_000
+
+def time_to_string(time_in_seconds: float) -> str:
+    minutes = int(time_in_seconds // MINUTE)
+    seconds = int(time_in_seconds % MINUTE)
+    millis = int((time_in_seconds - int(time_in_seconds)) * MILLISECOND)
+    return f"{minutes}m {seconds}s {millis}ms"
+
+def get_mongo_client() -> MongoClient:
+    uri = f"mongodb+srv://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWORD')}@{os.getenv('MONGO_HOST')}/?retryWrites=true&w=majority&appName={os.getenv('MONGO_APP_NAME')}"
+    print(f"Connecting to MongoDB: {uri}")
+    logger.getLogger('pymongo').setLevel(logger.WARNING)
+    return MongoClient(uri, server_api=ServerApi('1'))
+
+def get_actual_time() -> str:
+    return datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+def is_float(value):
+    float_pattern = re.compile(r'^-?\d+(\.\d+)?$')
+    return bool(float_pattern.match(value))
