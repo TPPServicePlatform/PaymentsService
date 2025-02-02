ea967940ce65dd5756928894032bb4dc0e01bb0c
ElianaHarriet <eharriet@fi.uba.ar>
2025-02-02 13:20:33 -0300

    Add geopy dependency and implement coupon validation logic

--- api_container/coupons_nosql.py
+++ api_container/coupons_nosql.py
@@ -20,19 +20,18 @@ class Coupons:
     """
     Coupons class that stores data in a MongoDB collection.
     Fields:
-    - id: int (unique) [pk]
-    - coupon_code: str (unique)
+    - id: str (unique) (coupon code)
     - discount_percent: float
     - max_discount: float (optional)
     - expiration_date: datetime
     - created_at: datetime
     - updated_at: datetime
-    - used_by: List[str] -> List of user ids that used the coupon, the idea is to avoid multiple uses of the same coupon by the same user
+    - used_by: Dict[str, str] -> List of user ids that used the coupon, the idea is to avoid multiple uses of the same coupon by the same user. It has the following structure: {'user_id': 'used_at'}
     - category_rules: List[str] (optional) -> List of categories that the coupon is valid for
     - service_rules: List[str] (optional) -> List of service ids that the coupon is valid for
     - provider_rules: List[str] (optional) -> List of provider ids that the coupon is valid for
     - location_rule: (longitude and latitude) (optional) -> Location where the coupon is valid (center point)
-    - max_distance: int (optional) -> Max distance from the location_rule where the coupon is valid
+    - max_distance: int (optional) -> Max distance from the location_rule where the coupon is valid (kilometers)
     - users_rules: List[str] (optional) -> List of user ids that the coupon is valid for
     """
 
@@ -59,14 +58,25 @@ class Coupons:
         self.collection.create_index([('uuid', ASCENDING)], unique=True)
         self.collection.create_index([('location', '2dsphere')])
     
-    def insert(self, coupon_code: str, discount_percent: float, max_discount: Optional[float], expiration_date: int, category_rules: Optional[List[str]], service_rules: Optional[List[str]], provider_rules: Optional[List[str]], location_rule: Optional[dict], max_distance: Optional[int], users_rules: Optional[List[str]]) -> Optional[str]:
+    def insert(self,
+               coupon_code: str,
+               discount_percent: float,
+               expiration_date: int,
+               max_discount: Optional[float] = None,
+               category_rules: Optional[List[str]] = None,
+               service_rules: Optional[List[str]] = None,
+               provider_rules: Optional[List[str]] = None,
+               location_rule: Optional[dict] = None,
+               max_distance: Optional[int] = None,
+               users_rules: Optional[List[str]] = None
+            ) -> bool:
         try:
             self.collection.insert_one({
                 'uuid': coupon_code,
                 'discount_percent': discount_percent,
                 'max_discount': max_discount,
                 'expiration_date': expiration_date,
-                'used_by': [],
+                'used_by': {},
                 'category_rules': category_rules,
                 'service_rules': service_rules,
                 'provider_rules': provider_rules,
@@ -76,13 +86,13 @@ class Coupons:
                 'created_at': get_actual_time(),
                 'updated_at': get_actual_time()
             })
-            return coupon_code
+            return True
         except DuplicateKeyError as e:
             logger.error(f"DuplicateKeyError: {e}")
-            return None
+            return False
         except OperationFailure as e:
             logger.error(f"OperationFailure: {e}")
-            return None
+            return False
         
     def get(self, coupon_code: str) -> Optional[Dict]:
         return self.collection.find_one({'uuid': coupon_code}) or None
@@ -99,3 +109,92 @@ class Coupons:
         except Exception as e:
             logger.error(f"Error updating service with uuid '{uuid}': {e}")
             return False
+        
+    def obtain_available_coupons(self,
+                                 user_id: str,
+                                 client_location: dict,
+                                 category: str,
+                                 service_id: str,
+                                 provider_id: str
+                                    ) -> List[Dict]:
+        pipeline = []
+
+        # Filter by expiration date
+        pipeline.append({'$match': {'expiration_date': {'$gte': get_actual_time()}}})
+
+        # Filter by category
+        # If the coupon has no category rules, it is valid for all categories
+        pipeline.append({'$match': {'$or': [{'category_rules': {'$exists': False}}, {'category_rules': None}, {'category_rules': {'$in': [category]}}]}})
+
+        # Filter by service
+        # If the coupon has no service rules, it is valid for all services
+        pipeline.append({'$match': {'$or': [{'service_rules': {'$exists': False}}, {'service_rules': None}, {'service_rules': {'$in': [service_id]}}]}})
+
+        # Filter by provider
+        # If the coupon has no provider rules, it is valid for all providers
+        pipeline.append({'$match': {'$or': [{'provider_rules': {'$exists': False}}, {'provider_rules': None}, {'provider_rules': {'$in': [provider_id]}}]}})
+
+        # Filter by location and max distance
+        # If the coupon has no location rule, it is valid for all locations
+        if not os.environ.get('MONGOMOCK'):
+            geo_near_stage = {
+                '$geoNear': {
+                    'near': {
+                        'type': 'Point',
+                        'coordinates': [client_location['longitude'], client_location['latitude']]
+                    },
+                    'distanceField': 'distance',
+                    'spherical': True
+                }
+            }
+            pipeline.append(geo_near_stage)
+
+            match_stage = {
+                '$match': {
+                    '$or': [{'max_distance': {'$exists': False}}, {'max_distance': None}, {'$expr': {'$lte': ['$distance', {'$multiply': ['$max_distance', 1000]}]}}] # Convert kilometers to meters
+                }
+            }
+            pipeline.append(match_stage)
+
+        # Filter by user
+        # If the coupon has no user rules, it is valid for all users
+        pipeline.append({'$match': {'$or': [{'users_rules': {'$exists': False}}, {'users_rules': None}, {'users_rules': {'$in': [user_id]}}]}})
+
+        # Filter by used_by (avoid multiple uses of the same coupon by the same user)
+        pipeline.append({'$match': {'used_by': {'$not': {'$elemMatch': {'$eq': user_id}}}}})
+
+        # Project only the necessary fields
+        pipeline.append({'$project': {'_id': 0, 'uuid': 1, 'discount_percent': 1, 'max_discount': 1, 'expiration_date': 1}})
+
+        return list(self.collection.aggregate(pipeline))
+    
+    def add_user_to_coupon(self, coupon_code: str, user_id: str) -> bool:
+        coupon = self.get(coupon_code)
+        if not coupon:
+            return False
+        used_by = {**coupon.get('used_by', {}), user_id: get_actual_time()}
+        try:
+            result = self.collection.update_one({'uuid': coupon_code}, {'$set': {'used_by': used_by}})
+            return result.modified_count > 0
+        except Exception as e:
+            logger.error(f"Error adding user '{user_id}' to coupon '{coupon_code}': {e}")
+            return False
+        
+    def add_item_to_rule(self, coupon_code: str, rule: str, item: str) -> bool:
+        # verify rule
+        coupon = self.get(coupon_code)
+        if not coupon or rule not in coupon or len(coupon[rule] or []) == 0:
+            return False
+        try:
+            result = self.collection.update_one({'uuid': coupon_code}, {'$push': {rule: item}})
+            return result.modified_count > 0
+        except Exception as e:
+            logger.error(f"Error adding item '{item}' to rule '{rule}' of coupon '{coupon_code}': {e}")
+            return False
+    
+    def print_all(self):
+        for coupon in self.collection.find():
+            print(coupon)
+            
+
+
--- lib/utils.py
+++ lib/utils.py
@@ -7,6 +7,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import logging as logger
 import re
+import geopy.distance
 
 DAY = 24 * 60 * 60
 HOUR = 60 * 60
@@ -31,3 +32,36 @@ def get_actual_time() -> str:
 def is_float(value):
     float_pattern = re.compile(r'^-?\d+(\.\d+)?$')
     return bool(float_pattern.match(value))
+
+def validate_fields(data: dict, required_fields: set, valid_fields: set) -> None:
+    extra_fields = set(data.keys()) - valid_fields
+    missing_fields = required_fields - set(data.keys())
+    if extra_fields:
+        raise HTTPException(status_code=400, detail=f"Extra fields: {extra_fields}")
+    if missing_fields:
+        raise HTTPException(status_code=400, detail=f"Missing fields: {missing_fields}")
+
+def validate_location(client_location, required_fields):
+    if type(client_location) == str:
+        if client_location.count(",") != 1:
+            raise HTTPException(status_code=400, detail="Invalid location (must be in the format 'longitude,latitude')")
+        client_location = client_location.split(",")
+        client_location = {"longitude": client_location[0], "latitude": client_location[1]}
+    elif type(client_location) == dict:
+        if not all([field in client_location for field in required_fields]):
+            missing_fields = required_fields - set(client_location.keys())
+            raise HTTPException(status_code=400, detail=f"Missing location fields: {', '.join(missing_fields)}")
+    else:
+        raise HTTPException(status_code=400, detail="Invalid location (must be a string or a dictionary)")
+    if not all([type(value) in [int, float] or is_float(value) for value in client_location.values()]):
+        raise HTTPException(status_code=400, detail="Invalid location (each value must be a float)")
+    client_location = {key: float(value) for key, value in client_location.items()}
+    return client_location
+
+def calculate_distance(location1: dict, location2: dict) -> float:
+    coords1 = (location1['latitude'], location1['longitude'])
+    coords2 = (location2['latitude'], location2['longitude'])
+    return geopy.distance.distance(coords1, coords2).km
+
+    
+
