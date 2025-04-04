6333ae92552ec85d4c4e0ef7fa56ba3c5615ca91
tomas <temanuel@fi.uba.ar>
2025-04-04 20:52:29 -0300

    get refund coupons and change logic

--- api_container/coupons_nosql.py
+++ api_container/coupons_nosql.py
@@ -14,6 +14,8 @@ MINUTE = 60
 MILLISECOND = 1_000
 
 # TODO: (General) -> Create tests for each method && add the required checks in each method
+
+
 class Coupons:
     """
     Coupons class that stores data in a MongoDB collection.
@@ -43,7 +45,7 @@ class Coupons:
             self.db = self.client[test_db or os.getenv('MONGO_DB')]
         self.collection = self.db['payments']
         self._create_collection()
-    
+
     def _check_connection(self):
         try:
             self.client.admin.command('ping')
@@ -55,7 +57,7 @@ class Coupons:
     def _create_collection(self):
         self.collection.create_index([('uuid', ASCENDING)], unique=True)
         self.collection.create_index([('location', '2dsphere')])
-    
+
     def insert(self,
                coupon_code: str,
                discount_percent: float,
@@ -67,7 +69,7 @@ class Coupons:
                location_rule: Optional[dict] = None,
                max_distance: Optional[int] = None,
                users_rules: Optional[List[str]] = None
-            ) -> bool:
+               ) -> bool:
         try:
             self.collection.insert_one({
                 'uuid': coupon_code,
@@ -91,46 +93,51 @@ class Coupons:
         except OperationFailure as e:
             logger.error(f"OperationFailure: {e}")
             return False
-        
+
     def get(self, coupon_code: str) -> Optional[Dict]:
         return self.collection.find_one({'uuid': coupon_code}) or None
-    
+
     def delete(self, coupon_code: str) -> bool:
         result = self.collection.delete_one({'uuid': coupon_code})
         return result.deleted_count > 0
-    
+
     def update(self, coupon_code: str, data: Dict) -> bool:
         data['updated_at'] = get_actual_time()
         try:
-            result = self.collection.update_one({'uuid': coupon_code}, {'$set': data})
+            result = self.collection.update_one(
+                {'uuid': coupon_code}, {'$set': data})
             return result.modified_count > 0
         except Exception as e:
             logger.error(f"Error updating service with uuid '{uuid}': {e}")
             return False
-        
+
     def obtain_available_coupons(self,
                                  user_id: str,
                                  client_location: dict,
                                  category: str,
                                  service_id: str,
                                  provider_id: str
-                                    ) -> List[Dict]:
+                                 ) -> List[Dict]:
         pipeline = []
 
         # Filter by expiration date
-        pipeline.append({'$match': {'expiration_date': {'$gte': get_actual_time()}}})
+        pipeline.append(
+            {'$match': {'expiration_date': {'$gte': get_actual_time()}}})
 
         # Filter by category
         # If the coupon has no category rules, it is valid for all categories
-        pipeline.append({'$match': {'$or': [{'category_rules': {'$exists': False}}, {'category_rules': None}, {'category_rules': {'$in': [category]}}]}})
+        pipeline.append({'$match': {'$or': [{'category_rules': {'$exists': False}}, {
+                        'category_rules': None}, {'category_rules': {'$in': [category]}}]}})
 
         # Filter by service
         # If the coupon has no service rules, it is valid for all services
-        pipeline.append({'$match': {'$or': [{'service_rules': {'$exists': False}}, {'service_rules': None}, {'service_rules': {'$in': [service_id]}}]}})
+        pipeline.append({'$match': {'$or': [{'service_rules': {'$exists': False}}, {
+                        'service_rules': None}, {'service_rules': {'$in': [service_id]}}]}})
 
         # Filter by provider
         # If the coupon has no provider rules, it is valid for all providers
-        pipeline.append({'$match': {'$or': [{'provider_rules': {'$exists': False}}, {'provider_rules': None}, {'provider_rules': {'$in': [provider_id]}}]}})
+        pipeline.append({'$match': {'$or': [{'provider_rules': {'$exists': False}}, {
+                        'provider_rules': None}, {'provider_rules': {'$in': [provider_id]}}]}})
 
         # Filter by location and max distance
         # If the coupon has no location rule, it is valid for all locations
@@ -149,35 +156,46 @@ class Coupons:
 
             match_stage = {
                 '$match': {
-                    '$or': [{'max_distance': {'$exists': False}}, {'max_distance': None}, {'$expr': {'$lte': ['$distance', {'$multiply': ['$max_distance', 1000]}]}}] # Convert kilometers to meters
+                    # Convert kilometers to meters
+                    '$or': [{'max_distance': {'$exists': False}}, {'max_distance': None}, {'$expr': {'$lte': ['$distance', {'$multiply': ['$max_distance', 1000]}]}}]
                 }
             }
             pipeline.append(match_stage)
 
         # Filter by user
         # If the coupon has no user rules, it is valid for all users
-        pipeline.append({'$match': {'$or': [{'users_rules': {'$exists': False}}, {'users_rules': None}, {'users_rules': {'$in': [user_id]}}]}})
+        pipeline.append({'$match': {'$or': [{'users_rules': {'$exists': False}}, {
+                        'users_rules': None}, {'users_rules': {'$in': [user_id]}}]}})
 
         # Filter by used_by (avoid multiple uses of the same coupon by the same user)
         # used_by is a dictionary with the following structure: {'user_id': 'used_at'}
-        pipeline.append({'$match': {'$or': [{'used_by': {'$exists': False}}, {'used_by': {}}, {'used_by.' + user_id: {'$exists': False}}]}})
+        pipeline.append({'$match': {'$or': [{'used_by': {'$exists': False}}, {
+                        'used_by': {}}, {'used_by.' + user_id: {'$exists': False}}]}})
 
         # Project only the necessary fields
-        pipeline.append({'$project': {'_id': 0, 'uuid': 1, 'discount_percent': 1, 'max_discount': 1, 'expiration_date': 1}})
+        pipeline.append({'$project': {'_id': 0, 'uuid': 1,
+                        'discount_percent': 1, 'max_discount': 1, 'expiration_date': 1}})
 
         return list(self.collection.aggregate(pipeline))
-    
-    def obtain_user_coupons(self,
-                                 user_id: str,
-                                 client_location: dict,
-                                    ) -> List[Dict]:
+
+    def get_refund_coupons(self, user_id: str) -> List[Dict]:
+        return list(self.collection.find({
+            'uuid': {'$regex': f'^REFUND_{user_id}_'},
+            f'used_by.{user_id}': {'$exists': False}
+        }, {
+            '_id': 0,
+            'uuid': 1,
+            'max_discount': 1,
+            'expiration_date': 1,
+            'discount_percent': 1,
+        }))
+
+    def get_all_coupons(self) -> List[Dict]:
+        return list(self.collection.find({}, {'_id': 0}))
+
+    def obtain_user_coupons(self, user_id: str, client_location: dict) -> List[Dict]:
         pipeline = []
 
-        # Filter by expiration date
-        pipeline.append({'$match': {'expiration_date': {'$gte': get_actual_time()}}})
-
-        # Filter by location and max distance
-        # If the coupon has no location rule, it is valid for all locations
         if not os.environ.get('MONGOMOCK'):
             geo_near_stage = {
                 '$geoNear': {
@@ -191,50 +209,86 @@ class Coupons:
             }
             pipeline.append(geo_near_stage)
 
-            match_stage = {
+            match_distance_stage = {
                 '$match': {
-                    '$or': [{'max_distance': {'$exists': False}}, {'max_distance': None}, {'$expr': {'$lte': ['$distance', {'$multiply': ['$max_distance', 1000]}]}}] # Convert kilometers to meters
+                    '$or': [
+                        {'max_distance': {'$exists': False}},
+                        {'max_distance': None},
+                        {'$expr': {
+                            '$lte': ['$distance', {'$multiply': ['$max_distance', 1000]}]
+                        }}
+                    ]
                 }
             }
-            pipeline.append(match_stage)
+            pipeline.append(match_distance_stage)
 
-        # Filter by user
-        # If the coupon has no user rules, it is valid for all users
-        pipeline.append({'$match': {'$or': [{'users_rules': {'$exists': False}}, {'users_rules': None}, {'users_rules': {'$in': [user_id]}}]}})
+        # ✅ El resto de los filtros sí pueden ir luego
+        # Filtro por expiración
+        pipeline.append({
+            '$match': {
+                'expiration_date': {'$gte': get_actual_time()}
+            }
+        })
 
-        # Filter by used_by (avoid multiple uses of the same coupon by the same user)
-        # used_by is a dictionary with the following structure: {'user_id': 'used_at'}
-        pipeline.append({'$match': {'$or': [{'used_by': {'$exists': False}}, {'used_by': {}}, {'used_by.' + user_id: {'$exists': False}}]}})
+        # Filtro por usuarios
+        pipeline.append({
+            '$match': {
+                '$or': [
+                    {'users_rules': {'$exists': False}},
+                    {'users_rules': None},
+                    {'users_rules': {'$in': [user_id]}}
+                ]
+            }
+        })
 
-        # Project only the necessary fields
-        pipeline.append({'$project': {'_id': 0, 'uuid': 1, 'discount_percent': 1, 'max_discount': 1, 'expiration_date': 1}})
+        # Filtro por cupones usados
+        pipeline.append({
+            '$match': {
+                '$or': [
+                    {'used_by': {'$exists': False}},
+                    {'used_by': {}},
+                    {f'used_by.{user_id}': {'$exists': False}}
+                ]
+            }
+        })
+
+        # Proyección final
+        pipeline.append({
+            '$project': {
+                '_id': 0,
+                'uuid': 1,
+                'discount_percent': 1,
+                'max_discount': 1,
+                'expiration_date': 1
+            }
+        })
 
         return list(self.collection.aggregate(pipeline))
-    
-    
+
     def add_user_to_coupon(self, coupon_code: str, user_id: str) -> bool:
         coupon = self.get(coupon_code)
         if not coupon:
             return False
         used_by = {**coupon.get('used_by', {}), user_id: get_actual_time()}
         try:
-            result = self.collection.update_one({'uuid': coupon_code}, {'$set': {'used_by': used_by}})
+            result = self.collection.update_one(
+                {'uuid': coupon_code}, {'$set': {'used_by': used_by}})
             return result.modified_count > 0
         except Exception as e:
-            logger.error(f"Error adding user '{user_id}' to coupon '{coupon_code}': {e}")
+            logger.error(
+                f"Error adding user '{user_id}' to coupon '{coupon_code}': {e}")
             return False
-        
+
     def add_item_to_rule(self, coupon_code: str, rule: str, item: str) -> bool:
         # verify rule
         coupon = self.get(coupon_code)
         if not coupon or rule not in coupon or len(coupon[rule] or []) == 0:
             return False
         try:
-            result = self.collection.update_one({'uuid': coupon_code}, {'$push': {rule: item}})
+            result = self.collection.update_one(
+                {'uuid': coupon_code}, {'$push': {rule: item}})
             return result.modified_count > 0
         except Exception as e:
-            logger.error(f"Error adding item '{item}' to rule '{rule}' of coupon '{coupon_code}': {e}")
+            logger.error(
+                f"Error adding item '{item}' to rule '{rule}' of coupon '{coupon_code}': {e}")
             return False
-            
-
-
--- api_container/payments_api.py
+++ api_container/payments_api.py
@@ -64,23 +64,29 @@ else:
 
 REQUIRED_LOCATION_FIELDS = {"longitude", "latitude"}
 
-REQUIRED_COUPON_CREATE_FIELDS = {'coupon_code', 'discount_percent', 'expiration_date'}
-VALID_COUPON_RULES = {'category_rules', 'service_rules', 'provider_rules', 'location_rule', 'max_distance', 'users_rules'}
-VALID_COUPON_CREATE_FIELDS = {'max_discount'} | VALID_COUPON_RULES | REQUIRED_COUPON_CREATE_FIELDS
+REQUIRED_COUPON_CREATE_FIELDS = {
+    'coupon_code', 'discount_percent', 'expiration_date'}
+VALID_COUPON_RULES = {'category_rules', 'service_rules',
+                      'provider_rules', 'location_rule', 'max_distance', 'users_rules'}
+VALID_COUPON_CREATE_FIELDS = {
+    'max_discount'} | VALID_COUPON_RULES | REQUIRED_COUPON_CREATE_FIELDS
 REQUIRED_REFUND_FIELDS = {'user_id', 'amount'}
 
 REQUIRED_TRANSACTION_FIELDS = {'points', 'description'}
 
-POINTS_PER_CASH = lambda cash: cash / 10
-CASH_COUPON_POINTS_NEEDED = lambda cash_discount: cash_discount * 10
-DISCOUNT_COUPON_POINTS_NEEDED = lambda discount: discount * 100
 
-YEAR = 365 # Days
+def POINTS_PER_CASH(cash): return cash / 10
+def CASH_COUPON_POINTS_NEEDED(cash_discount): return cash_discount * 10
+def DISCOUNT_COUPON_POINTS_NEEDED(discount): return discount * 100
+
+
+YEAR = 365  # Days
 
 starting_duration = time_to_string(time.time() - time_start)
 logger.info(f"Payments API started in {starting_duration}")
 
 # TODO: (General) -> Create tests for each endpoint && add the required checks in each endpoint
+
 
 @app.get("/pay/{service_id}/paymentlink")
 async def create_payment_link(
@@ -112,40 +118,50 @@ async def create_payment_link(
         return {"url": checkout_session.url}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
+
 @app.post("/pay/{user_id}/paymentdone")
 def payment_done(user_id: str, body: dict):
     validate_fields(body, {"amount", "description"}, {"amount", "description"})
     if not loyalty_manager.register_client_payment(user_id, body['amount'], body['description']):
-        raise HTTPException(status_code=500, detail="Failed to register the payment")
+        raise HTTPException(
+            status_code=500, detail="Failed to register the payment")
     return {"status": "ok"}
+
 
 @app.post("/pay/{provider_id}/paymentreceived")
 def payment_received(provider_id: str, body: dict):
     # Add a third party app to pay to the provider
-    
+
     validate_fields(body, {"amount", "description"}, {"amount", "description"})
     if not loyalty_manager.register_provider_payment(provider_id, body['amount'], body['description']):
-        raise HTTPException(status_code=500, detail="Failed to register the payment")
+        raise HTTPException(
+            status_code=500, detail="Failed to register the payment")
     return {"status": "ok"}
-    
+
+
 @app.post("/coupons/create")
 def create_coupon(body: dict):
-    validate_fields(body, REQUIRED_COUPON_CREATE_FIELDS, VALID_COUPON_CREATE_FIELDS)
-    
+    validate_fields(body, REQUIRED_COUPON_CREATE_FIELDS,
+                    VALID_COUPON_CREATE_FIELDS)
+
     # At least one ruled is needed
     if not any([field in body for field in VALID_COUPON_RULES]):
-        raise HTTPException(status_code=400, detail="At least one rule is needed")
+        raise HTTPException(
+            status_code=400, detail="At least one rule is needed")
     if bool('location_rule' in body) != bool('max_distance' in body):
-        raise HTTPException(status_code=400, detail="Both location_rule and max_distance are needed, or none of them")
+        raise HTTPException(
+            status_code=400, detail="Both location_rule and max_distance are needed, or none of them")
     if 'location_rule' in body:
-        location = validate_location(body['location_rule'], REQUIRED_LOCATION_FIELDS)
+        location = validate_location(
+            body['location_rule'], REQUIRED_LOCATION_FIELDS)
     if body['discount_percent'] <= 0 or body['discount_percent'] > 100:
         raise HTTPException(status_code=400, detail="Invalid discount percent")
 
     if coupons_manager.get(body['coupon_code']):
-        raise HTTPException(status_code=400, detail="Coupon code already exists")
-    
+        raise HTTPException(
+            status_code=400, detail="Coupon code already exists")
+
     if not coupons_manager.insert(
         coupon_code=body.get('coupon_code'),
         discount_percent=body.get('discount_percent'),
@@ -158,16 +174,18 @@ def create_coupon(body: dict):
         max_distance=body.get('max_distance'),
         users_rules=body.get('users_rules')
     ):
-        raise HTTPException(status_code=500, detail="Failed to create the coupon")
-    
+        raise HTTPException(
+            status_code=500, detail="Failed to create the coupon")
+
     return {"status": "ok"}
+
 
 @app.post("/coupons/new_refund")
 def create_refund_coupon(body: dict):
     validate_fields(body, REQUIRED_REFUND_FIELDS, REQUIRED_REFUND_FIELDS)
     if body['amount'] <= 0:
         raise HTTPException(status_code=400, detail="Amount must be positive")
-    
+
     code = f"REFUND_{body['user_id']}_{time.time()}"
     if not coupons_manager.insert(
         coupon_code=code,
@@ -176,20 +194,31 @@ def create_refund_coupon(body: dict):
         expiration_date=get_timestamp_after_days(100*YEAR),
         users_rules=[body['user_id']]
     ):
-        raise HTTPException(status_code=500, detail="Failed to create the coupon")
+        raise HTTPException(
+            status_code=500, detail="Failed to create the coupon")
 
-    send_notification(mobile_token_manager, body['user_id'], "Refund coupon", f"Refund coupon of {body['amount']} created")
+    send_notification(mobile_token_manager,
+                      body['user_id'], "Refund coupon", f"Refund coupon of {body['amount']} created")
     return {"status": "ok", "coupon_code": code}
+
 
 @app.delete("/coupons/delete/{coupon_code}")
 def delete_coupon(coupon_code: str):
     if not coupons_manager.get(coupon_code):
         raise HTTPException(status_code=404, detail="Coupon not found")
-    
+
     if not coupons_manager.delete(coupon_code):
-        raise HTTPException(status_code=500, detail="Failed to delete the coupon")
-    
+        raise HTTPException(
+            status_code=500, detail="Failed to delete the coupon")
+
     return {"status": "ok"}
+
+
+@app.get("/coupons/all_coupons")
+def get_all_coupons():
+    all_coupons = coupons_manager.get_all_coupons()
+    return {"status": "ok", "coupons": all_coupons}
+
 
 @app.get("/coupons")
 def obtain_available_coupons(
@@ -207,8 +236,9 @@ def obtain_available_coupons(
         service_id=service_id,
         provider_id=provider_id
     )
-    
+
     return {"status": "ok", "coupons": available_coupons}
+
 
 @app.get("/coupons/all")
 def obtain_user_coupons(
@@ -220,57 +250,72 @@ def obtain_user_coupons(
         user_id=user_id,
         client_location=location
     )
-    return {"status": "ok", "coupons": all_coupons} 
-    
+    return {"status": "ok", "coupons": all_coupons}
+
+
+@app.get("/coupons/refund")
+def get_refund_coupons(user_id: str):
+    refund_coupons = coupons_manager.get_refund_coupons(user_id)
+    return {"status": "ok", "refund_coupons": refund_coupons}
+
 
 @app.put("/coupons/activate/{coupon_code}/{user_id}")
 def activate_coupon(coupon_code: str, user_id: str, body: dict):
     needed = {'client_location', 'category', 'service_id', 'provider_id'}
     if not all([field in body for field in needed]):
         missing_fields = needed - set(body.keys())
-        raise HTTPException(status_code=400, detail=f"Missing fields: {', '.join(missing_fields)}")
-    
+        raise HTTPException(
+            status_code=400, detail=f"Missing fields: {', '.join(missing_fields)}")
+
     coupon = coupons_manager.get(coupon_code)
     if not coupon:
         raise HTTPException(status_code=404, detail="Coupon not found")
-    
-    success, message = verify_coupon_rules(coupon, user_id, body['category'], body['service_id'], body['provider_id'], body['client_location'])
+
+    success, message = verify_coupon_rules(
+        coupon, user_id, body['category'], body['service_id'], body['provider_id'], body['client_location'])
     if not success:
         raise HTTPException(status_code=400, detail=message)
-    
-    
+
     if not coupons_manager.add_user_to_coupon(coupon_code, user_id):
-        raise HTTPException(status_code=500, detail="Failed to activate the coupon")
-    
+        raise HTTPException(
+            status_code=500, detail="Failed to activate the coupon")
+
     if not loyalty_manager.register_coupon_use(user_id, coupon_code, f"Used coupon {coupon_code}"):
         coupons_manager.remove_user_from_coupon(coupon_code, user_id)
-        raise HTTPException(status_code=500, detail="Failed to register the coupon")
-    
+        raise HTTPException(
+            status_code=500, detail="Failed to register the coupon")
+
     return {"status": "ok", "discount_percent": coupon['discount_percent'], "max_discount": coupon['max_discount']}
-    
+
+
 @app.put("/loyalty/sum_points/{user_id}")
 def add_loyalty_transaction(user_id: str, body: dict):
-    validate_fields(body, REQUIRED_TRANSACTION_FIELDS, REQUIRED_TRANSACTION_FIELDS)
+    validate_fields(body, REQUIRED_TRANSACTION_FIELDS,
+                    REQUIRED_TRANSACTION_FIELDS)
     if body['points'] <= 0:
         raise HTTPException(status_code=400, detail="Points must be positive")
-    
+
     if not loyalty_manager.add_transaction(user_id, body['points'], body['description']):
-        raise HTTPException(status_code=500, detail="Failed to add the transaction")
-    
+        raise HTTPException(
+            status_code=500, detail="Failed to add the transaction")
+
     return {"status": "ok"}
+
 
 @app.put("/loyalty/use_points/cash_coupon/{user_id}")
 def buy_cash_coupon(user_id: str, body: dict):
     validate_fields(body, {"CASH_DISCOUNT"}, {"CASH_DISCOUNT"})
     if body['CASH_DISCOUNT'] <= 0:
-        raise HTTPException(status_code=400, detail="Discount must be positive")
-    
+        raise HTTPException(
+            status_code=400, detail="Discount must be positive")
+
     points_needed = CASH_COUPON_POINTS_NEEDED(body['CASH_DISCOUNT'])
     total_points = loyalty_manager.get_total_points(user_id)
     if not total_points or total_points < points_needed:
         raise HTTPException(status_code=400, detail="Not enough points")
-    
-    coupon_code=f"CASH_{user_id}_{body['CASH_DISCOUNT']}_{time.time()}" # Unique coupon code
+
+    # Unique coupon code
+    coupon_code = f"CASH_{user_id}_{body['CASH_DISCOUNT']}_{time.time()}"
     if not coupons_manager.insert(
         coupon_code=coupon_code,
         discount_percent=100,
@@ -278,51 +323,59 @@ def buy_cash_coupon(user_id: str, body: dict):
         expiration_date=get_timestamp_after_days(YEAR),
         users_rules=[user_id]
     ):
-        raise HTTPException(status_code=500, detail="Failed to create the coupon")
-    
+        raise HTTPException(
+            status_code=500, detail="Failed to create the coupon")
+
     if not loyalty_manager.add_transaction(user_id, -points_needed, f"Bought cash coupon of {body['CASH_DISCOUNT']} ({coupon_code})"):
         coupons_manager.delete(coupon_code)
         raise HTTPException(status_code=500, detail="Failed to use the points")
-    
+
     return {"status": "ok", "coupon_code": coupon_code}
-    
+
+
 @app.put("/loyalty/use_points/discount_coupon/{user_id}")
 def buy_discount_coupon(user_id: str, body: dict):
     validate_fields(body, {"DISCOUNT"}, {"DISCOUNT"})
     if body['DISCOUNT'] <= 0 or body['DISCOUNT'] > 100:
         raise HTTPException(status_code=400, detail="Invalid discount")
-    
+
     points_needed = DISCOUNT_COUPON_POINTS_NEEDED(body['DISCOUNT'])
     total_points = loyalty_manager.get_total_points(user_id)
     if not total_points or total_points < points_needed:
         raise HTTPException(status_code=400, detail="Not enough points")
-    
-    coupon_code=f"DISCOUNT_{user_id}_{body['DISCOUNT']}perc_{time.time()}" # Unique coupon code
+
+    # Unique coupon code
+    coupon_code = f"DISCOUNT_{user_id}_{body['DISCOUNT']}perc_{time.time()}"
     if not coupons_manager.insert(
         coupon_code=coupon_code,
         discount_percent=body['DISCOUNT'],
         expiration_date=get_timestamp_after_days(YEAR),
         users_rules=[user_id]
     ):
-        raise HTTPException(status_code=500, detail="Failed to create the coupon")
-    
+        raise HTTPException(
+            status_code=500, detail="Failed to create the coupon")
+
     if not loyalty_manager.add_transaction(user_id, -points_needed, f"Bought discount coupon of {body['DISCOUNT']}% ({coupon_code})"):
         coupons_manager.delete(coupon_code)
         raise HTTPException(status_code=500, detail="Failed to use the points")
-    
+
     return {"status": "ok", "coupon_code": coupon_code}
+
 
 @app.get("/loyalty/points/{user_id}")
 def obtain_user_points(user_id: str):
     total_points = loyalty_manager.get_total_points(user_id)
     if total_points == None:
-        raise HTTPException(status_code=404, detail="User does not have loyalty points yet")
+        raise HTTPException(
+            status_code=404, detail="User does not have loyalty points yet")
     expiring_dates = loyalty_manager.get_expiring_points(user_id)
     return {"status": "ok", "total_points": total_points, "expiring_dates": expiring_dates}
+
 
 @app.get("/loyalty/history/{user_id}")
 def obtain_user_history(user_id: str):
     history = loyalty_manager.get_history(user_id)
     if history == None:
-        raise HTTPException(status_code=404, detail="User does not have loyalty points yet")
+        raise HTTPException(
+            status_code=404, detail="User does not have loyalty points yet")
     return {"status": "ok", "history": history}
